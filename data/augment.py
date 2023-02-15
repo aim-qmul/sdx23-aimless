d5c96e70f9979f532bd221fe1dc02180ccfd3fc3
Chin-Yun Yu <cy006@wynne.eecs.qmul.ac.uk>
2023-02-15 13:59:10 +0000

    fix: treat each channel separately

--- data/augment.py
+++ data/augment.py
@@ -823,19 +823,11 @@ class RandomPan(CPUBase):
     def _transform(self, x: torch.Tensor):
         """Constant power panning"""
         assert x.ndim == 2 and x.shape[0] == 2, "x must be stereo"
-        theta = rand(self.min_pan, self.max_pan) * np.pi / 2
+        theta = rand(self.min_pan, self.max_pan) * np.pi / 4
         x = x * 0.707  # normalize to prevent clipping
         left_x, right_x = x[0], x[1]
-        if theta > 0:
-            # pan left channel only
-            right_gain = np.sin(theta)
-            left_gain = np.cos(theta)
-            left_x = left_gain * left_x
-            right_x = right_gain * left_x + right_x
-        else:
-            # pan right channel only
-            left_gain = np.sin(-theta)
-            right_gain = np.cos(-theta)
-            right_x = right_gain * right_x
-            left_x = left_gain * right_x + left_x
+        cos_theta = np.cos(theta)
+        sin_theta = np.sin(theta)
+        left_x = left_x * (cos_theta - sin_theta)
+        right_x = right_x * (cos_theta + sin_theta)
         return torch.stack([left_x, right_x], dim=0)
