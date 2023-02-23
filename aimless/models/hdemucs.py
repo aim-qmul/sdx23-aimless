95fbb250153be0a21340839fc72763b0b6b0062e
csteinmetz1 <csteinmetz1@gmail.com>
2023-02-23 09:59:13 +0000

    add flag to download weights from web or not

--- aimless/models/hdemucs.py
+++ aimless/models/hdemucs.py
@@ -4,10 +4,13 @@ from torchaudio.pipelines import HDEMUCS_HIGH_MUSDB_PLUS
 
 
 class PretrainedHDemucs(torch.nn.Module):
-    def __init__(self, sources) -> None:
+    def __init__(self, sources, download_weights: bool = False) -> None:
         super().__init__()
-        bundle = HDEMUCS_HIGH_MUSDB_PLUS
-        self.model = bundle.get_model()
+        if download_weights:
+            bundle = HDEMUCS_HIGH_MUSDB_PLUS
+            self.model = bundle.get_model()
+        else:
+            self.model = torchaudio.models.hdemucs_high()
 
     def forward(self, x: torch.Tensor):
         return self.model(x)
